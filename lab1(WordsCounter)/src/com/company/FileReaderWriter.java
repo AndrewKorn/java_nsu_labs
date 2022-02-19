39312fea416c05b96ccf9c4816e09c0ac2b229b5
AndrewKorn <a.korneshchuk1@g.nsu.ru>
2022-02-19 17:12:10 +0700

    trying to keep SRP, rename CreateSetOfWords -> countWords, little rework divideString, add closing in writeCSVFile

---
+++ lab1(WordsCounter)/src/com/company/FileReaderWriter.java
@@ -0,0 +1,90 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReaderWriter {
+    public static List<String> divideString(String line) {
+        int beginIdx = 0;
+        int wordLen = 0;
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < line.length(); ++i) {
+            if (Character.isLetterOrDigit(line.charAt(i))) {
+                ++wordLen;
+            }
+            else {
+                if (wordLen != 0) {
+                    String newWord = line.substring(beginIdx, beginIdx + wordLen);
+                    list.add(newWord);
+                }
+                beginIdx = i + 1;
+                wordLen = 0;
+            }
+        }
+        if (wordLen != 0) {
+            String newWord = line.substring(beginIdx, beginIdx + wordLen);
+            list.add(newWord);
+        }
+        return list;
+    }
+
+    public static List<String> readFile(String inputFile) {
+        BufferedReader reader = null;
+        List<String> list = new ArrayList<>();
+        try {
+            reader = new BufferedReader(new FileReader(inputFile));
+            while (reader.ready()) {
+                String line = reader.readLine();
+                list.addAll(divideString(line));
+            }
+            return list;
+        }
+        catch (IOException e) {
+            System.err.println("Error while reading file: " + e.getLocalizedMessage());
+        }
+        finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace(System.err);
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void writeCSVFile(String outputFile, List<WordCountPair> list, int wordsCount) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(outputFile, false);
+            for (WordCountPair pair : list) {
+                writer.write(
+                        pair.getWord() + ';'
+                                + pair.getCount() + ';'
+                                + (double)pair.getCount() / wordsCount * 100 + '\n'
+                );
+            }
+            writer.flush();
+        }
+        catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+        finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace(System.err);
+                }
+            }
+        }
+    }
+
+}
